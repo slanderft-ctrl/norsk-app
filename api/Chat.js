5c5961869905aaee1dca69232e7e9d1eeb862342
slanderft-ctrl <slander.ft@gmail.com>
2026-04-29 23:41:47 +0200

    bugs fixed

--- api/Chat.js
+++ api/Chat.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
                 cache_control: { type: "ephemeral" }
             }
         ],
-        messeges,
+        messages,
         }),
     })
   
